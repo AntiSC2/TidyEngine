07e8aa053968b81e17cd25be443abdbfde96fb99
Jakob Sinclair <jakob.sinclair99@gmail.com>
2015-08-12 18:13:38 +0200

    Started work on a new render system.

---
+++ src/Graphics/draw.h
@@ -0,0 +1,38 @@
+/*
+Copyright (C) 2015 Jakob Sinclair
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "screen.h"
+#include "renderable.h"
+#include <SDL2/SDL.h>
+#include <string>
+#include <vector>
+
+class Draw {
+    public:
+        Draw();
+	~Draw();
+
+        void begin();
+	void draw(Renderable& sprite);
+	void drawText(std::string text, std::string font, glm::vec2 position,
+	              glm::vec2 scale, SDL_Color color);
+	void end();
+	void render(Screen& screen);
+
+    private:
+        std::vector<Renderable> m_Glyphs;
+};
