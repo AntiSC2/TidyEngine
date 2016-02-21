7d048807ac05fb5d3022e212d6c82c7672eb8e59
Jakob Sinclair <jakob.sinclair99@gmail.com>
2016-02-21 16:42:50 +0100

    Revert "Can now build with CMake under Windows."
    This reverts commit b6585a9890928dcdd7028bbab0904ba9d7e7a1b7.

--- libTidyEngine/screen.cpp
+++ libTidyEngine/screen.cpp
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "screen.hpp"
+#include "screen.h"
 #include <stdio.h>
 
 Screen::Screen() {
