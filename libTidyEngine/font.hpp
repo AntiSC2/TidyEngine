de0e33786d95701bd008e45121ef44e2f7e72cf4
Jakob Sinclair <sinclair.jakob@openmailbox.org>
2016-12-30 20:44:07 +0100

    libTidyEngine: started adding font support
    Signed-off-by: Jakob Sinclair <sinclair.jakob@openmailbox.org>

---
+++ libTidyEngine/font.hpp
@@ -0,0 +1,41 @@
+/*
+TidyEngine
+Copyright (C) 2016 Jakob Sinclair
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Contact the author at: jakob.sinclair99@gmail.com
+*/
+
+#pragma once
+
+#include <ft2build.h>
+#include <freetype/freetype.h>
+#include <freetype/ftglyph.h>
+#include <freetype/ftoutln.h>
+#include <freetype/fttrigon>
+#include <GL/glew.h>
+#include <memory>
+
+class Font {
+public:
+	Font();
+	virtual ~Font();
+
+	void Init();
+	void Clean();
+	
+private:
+	float m_Height = 0.0f;
+	std::shared_ptr<GLuint> m_TextureIDs;
+}
