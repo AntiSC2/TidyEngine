a4346626bf059d54ee34017aebd29230933e2b93
Jakob Sinclair <sinclair.jakob@openmailbox.org>
2017-08-28 01:20:41 +0200

    Added system class
    Signed-off-by: Jakob Sinclair <sinclair.jakob@openmailbox.org>

---
+++ libTidyEngine/system.hpp
@@ -0,0 +1,34 @@
+/*
+TidyEngine
+Copyright (C) 2016 Jakob Sinclair
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Contact the author at: jakob.sinclair99@gmail.com
+*/
+
+#pragma once
+
+class EntityManager;
+
+class System {
+public:
+	System();
+	virtual ~System();
+
+	virtual void Update(double delta) = 0;
+	void SetEntityManager(EntityManager *manager);
+	EntityManager *GetEntityManager();
+protected:
+	EntityManager *Entities = nullptr;
+};
