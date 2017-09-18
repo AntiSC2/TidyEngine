27820f028a64f12aa6291ecfbb083e3de45107b2
Jakob Sinclair <sinclair.jakob@openmailbox.org>
2017-09-18 23:56:41 +0200

    Fixed issue #24 on Windows
    Signed-off-by: Jakob Sinclair <sinclair.jakob@openmailbox.org>

--- libTidyEngine/camera.hpp
+++ libTidyEngine/camera.hpp
@@ -53,9 +53,9 @@ private:
 	bool m_Update = false;
 	uint16_t m_Width = 0;
 	uint16_t m_Height = 0;
-	glm::mat4 m_Model;
-	glm::mat4 m_View;
-	glm::mat4 m_Projection;
+	glm::mat4 m_Model = glm::mat4(1.0f);
+	glm::mat4 m_View = glm::mat4(1.0f);
+	glm::mat4 m_Projection = glm::mat4(1.0f);
 	glm::vec3 m_Position = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::vec3 m_Dir = glm::vec3(0.0f, 0.0f, -1.0f);
 };
