b070be994acec6bd8f965eb4ca25819bed340dd4
Jakob Sinclair <sinclair.jakob@mailbox.org>
2018-02-03 00:42:35 +0100

    Add incomplete 3D demo
    Signed-off-by: Jakob Sinclair <sinclair.jakob@mailbox.org>

---
+++ 3Ddemo/game.hpp
@@ -0,0 +1,38 @@
+﻿/*
+Copyright (C) 2016 Jakob Sinclair
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+
+#pragma once
+#include "core.hpp"
+#include "model.hpp"
+
+class Game : public Core {
+public:
+	Game();
+	~Game();
+private:
+	bool Init();
+	void Update(double delta);
+	Model *m_Model;
+	glm::vec3 m_Pos = glm::vec3(0.0f, 15.0f, 10.0f);
+	glm::vec3 m_Front = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 m_Up = glm::vec3(0.0f, 1.0f, 0.0f);
+	float m_LastX = 0.0f;
+	float m_LastY = 0.0f;
+	float m_Yaw = 0.0f;
+	float m_Pitch = 0.0f;
+};
